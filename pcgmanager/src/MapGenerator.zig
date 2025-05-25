c43fc6c4829adea4850f8ff0af209a716ca5fe87
olafmustafar <soarthur@hotmail.com>
2025-05-25 14:27:48 -0300

    feat: progress worker implementation

--- pcgmanager/src/MapGenerator.zig
+++ pcgmanager/src/MapGenerator.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const Context = @import("Context.zig");
-const Queue = @import("queue.zig").Queue;
 
 const Self = @This();
 
@@ -29,58 +28,127 @@ const Chunk = struct {
 
 const Params = struct {};
 
-const WorkerCtx = struct {
-    queue: Queue(InstructionTag),
-    instruction: Instruction,
-    on_finish: *const fn (self: *WorkerCtx, position: Position, chunk: Chunk) void,
-};
+const Worker = struct {
+    const FinishFn = *const fn (pos: Position, chunk: Chunk) void;
+    const SharedContext = struct {
+        const State = enum { wait_instr, work, finish };
+        instruction: Instruction,
+        state: State,
+        on_finish: FinishFn,
+        cvar: std.Thread.Condition,
+        mutex: std.Thread.Mutex,
+    };
 
-cache: std.ArrayHashMap(Position, Chunk),
-params: Params,
-context: *Context,
-instructions: Queue(InstructionTag),
-workers: std.ArrayList(std.Thread),
-mutex: std.Thread.Mutex,
+    shared_context: *SharedContext,
+    thread: std.Thread,
+    allocator: std.mem.Allocator,
 
-pub fn init(context: *Context, thread_count: u8, allocator: std.mem.Allocator) !Self {
-    var self = Self{};
-    self.instructions = .init(.init(allocator));
-    self.cache = .init(allocator);
-    self.workers = try .init(allocator);
-    self.context = context;
-    self.params = .{};
-    self.mutex = .{};
+    fn init(allocator: std.mem.Allocator, on_finish: FinishFn) !Worker {
+        const shared = try allocator.create(SharedContext);
+        shared.* = .{
+            .instruction = undefined,
+            .state = .wait_instr,
+            .on_finish = on_finish,
+            .cvar = .{},
+            .mutex = .{},
+        };
 
-    for (0..thread_count) |_| {
-        const thread = try std.Thread.spawn(.{}, worker_function, .{});
-        self.workers.append(thread);
+        return .{
+            .thread = try std.Thread.spawn(.{}, worker_function, .{shared}),
+            .shared_context = shared,
+            .allocator = allocator,
+        };
     }
 
-    return .{};
+    fn deinit(self: *Worker) void {
+        const shared = self.shared_context;
+        {
+            shared.mutex.lock();
+            defer shared.mutex.unlock();
+            shared.state = .finish;
+        }
+        self.thread.join();
+        self.allocator.destroy(shared);
+    }
+
+    fn start(self: *Worker, instruction: Instruction) void {
+        var shared = self.shared_context;
+        {
+            shared.mutex.lock();
+            defer shared.mutex.unlock();
+            shared.instruction = instruction;
+            shared.state = 1;
+        }
+        shared.cvar.signal();
+    }
+
+    fn worker_function(ctx: *SharedContext) void {
+        ctx.mutex.lock();
+        defer ctx.mutex.unlock();
+        while (true) {
+            switch (ctx.state) {
+                .wait_instr => continue,
+                .work => worker_generate(ctx),
+                .finish => break,
+            }
+            ctx.cvar.wait(&ctx.mutex);
+        }
+    }
+
+    fn worker_generate(ctx: *SharedContext) void {
+        const chunk = Chunk{};
+        const pos = ctx.instruction.generate.position;
+        std.debug.print("Generating {} on thread {}", .{ pos, @intCast(std.Thread.getCurrentId()) });
+        ctx.on_finish(pos, chunk);
+        ctx.state = .wait_instr;
+    }
+};
+
+const Data = struct {
+    cache: std.ArrayHashMap(Position, Chunk),
+    params: Params,
+    context: *Context,
+    queue: std.ArrayList(Instruction),
+    workers: std.ArrayList(std.Thread),
+    mutex: std.Thread.Mutex,
+};
+
+data: *Data,
+allocator: std.mem.Allocator,
+
+pub fn init(context: *Context, worker_count: u8, allocator: std.mem.Allocator) !Self {
+    var data = try allocator.create(Data);
+    data.* = Data{
+        .queue = .init(allocator),
+        .cache = .init(allocator),
+        .workers = .init(allocator),
+        .context = context,
+        .params = .{},
+        .mutex = .{},
+    };
+
+    for (0..worker_count) |_|
+        data.workers.append(Worker.init(allocator, handle_generated));
+
+    return .{ .allocator = allocator, .data = data };
 }
 
 pub fn deinit(self: *Self) void {
-    for (self.workers.items) |worker|
-        worker.join();
-    self.workers.deinit();
-    self.cache.deinit();
-    self.instructions.deinit();
+    var data = self.data;
+    for (&data.workers.items) |*worker|
+        worker.deinit();
+    data.workers.deinit();
+    data.cache.deinit();
+    data.queue.deinit();
+
+    self.allocator.destroy(data);
+    self.* = undefined;
 }
 
 pub fn generate(self: Self, instruction: Instruction) void {
-    self.instructions.enqueue(instruction);
+    self.data.queue.insert(0, instruction);
 }
 
-fn worker_function(context: WorkerCtx) void {
-    const chunk = Chunk{};
-    const pos = instruction.generate.position;
-    std.debug.print("Generating {} on thread {}", .{ pos, @intCast(std.Thread.getCurrentId()) });
-    self.handle_generated(chunk);
-}
-
-fn handle_generated(self: *Self, position: Position, chunk: Chunk) void {
-    self.mutex.lock();
-    defer self.mutex.unlock();
-
-    self.cache.put(position, chunk);
+fn handle_generated(position: Position, chunk: Chunk) void {
+    self.data.cache.put(position, chunk);
 }
